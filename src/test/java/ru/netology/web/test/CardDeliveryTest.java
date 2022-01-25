446bac4088f4cce09f17e52b322c87ab6e70f1bf
Aleksey Stepin <diver911@yandex.ru>
2022-01-25 04:20:55 +0300

    adding tests files

---
+++ src/test/java/ru/netology/web/test/CardDeliveryTest.java
@@ -0,0 +1,49 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.web.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static java.time.Duration.ofSeconds;
+
+public class CardDeliveryTest {
+
+    @Test
+    void shouldFillingApplication() {
+        open("http://localhost:9999");
+
+        int firstMeet = DataGenerator.FillingForm.generateRandomNumber(4, 7);
+        int secondMeet = DataGenerator.FillingForm.generateRandomNumber(firstMeet + 5, firstMeet + 14);
+
+        $("[data-test-id='city'] input").setValue(DataGenerator.FillingForm.generateCustomerData("ru")
+                .getCity());
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(DataGenerator.FillingForm.generateMeetingDay(firstMeet));
+        $("[data-test-id='name'] input").setValue(DataGenerator.FillingForm.generateCustomerData("ru")
+                .getName());
+        $("[data-test-id='phone'] input").setValue(DataGenerator.FillingForm.generateCustomerData("ru")
+                .getPhone());
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $(withText("Запланировать")).click();
+        $("[data-test-id='success-notification']").shouldBe(visible, ofSeconds(15));
+        $("[data-test-id='success-notification']>.notification__content")
+                .shouldHave(text("Встреча успешно запланирована на " + DataGenerator.FillingForm.generateMeetingDay
+                        (firstMeet)));
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(DataGenerator.FillingForm.generateMeetingDay(secondMeet));
+        $(withText("Запланировать")).click();
+        $("[data-test-id='replan-notification']").shouldBe(visible);
+        $("[data-test-id='replan-notification']>.notification__content").
+                shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $(withText("Перепланировать")).click();
+        $("[data-test-id='success-notification']>.notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.FillingForm
+                        .generateMeetingDay(secondMeet)), Duration.ofSeconds(15));
+    }
+}
