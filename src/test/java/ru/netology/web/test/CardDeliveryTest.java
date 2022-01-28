f301f09118ea1d4bb4c9cb82f1b33a52f0c20c37
Aleksey Stepin <diver911@yandex.ru>
2022-01-28 04:13:30 +0300

    adding tests files

--- src/test/java/ru/netology/web/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/test/CardDeliveryTest.java
@@ -15,37 +15,33 @@ import static java.time.Duration.ofSeconds;
 
 public class CardDeliveryTest {
 
-
-
-
     @Test
     void shouldFillingApplication() {
         open("http://localhost:9999");
         val validUser = DataGenerator.FillingForm.generateCustomerData("ru");
         val firstMeet = DataGenerator.FillingForm.generateRandomNumber(4, 7);
-        val meeting1 = DataGenerator.FillingForm.generateMeetingDay(firstMeet);
+        val oldMeetingDate = DataGenerator.FillingForm.generateMeetingDay(firstMeet);
         val secondMeet = DataGenerator.FillingForm.generateRandomNumber(firstMeet + 5, firstMeet + 14);
-        val meeting2 = DataGenerator.FillingForm.generateMeetingDay(secondMeet);
+        val newMeetingDate = DataGenerator.FillingForm.generateMeetingDay(secondMeet);
 
         $("[data-test-id='city'] input").setValue(validUser.getCity());
-
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[placeholder='Дата встречи']").setValue(meeting1);
+        $("[placeholder='Дата встречи']").setValue(oldMeetingDate);
         $("[data-test-id='name'] input").setValue(validUser.getName());
         $("[data-test-id='phone'] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement] .checkbox__box").click();
         $(withText("Запланировать")).click();
         $("[data-test-id='success-notification']").shouldBe(visible, ofSeconds(15));
         $("[data-test-id='success-notification']>.notification__content")
-                .shouldHave(text("Встреча успешно запланирована на " + (meeting1)));
+                .shouldHave(text("Встреча успешно запланирована на " + (oldMeetingDate)));
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[placeholder='Дата встречи']").setValue(meeting2);
+        $("[placeholder='Дата встречи']").setValue(newMeetingDate);
         $(withText("Запланировать")).click();
         $("[data-test-id='replan-notification']").shouldBe(visible);
         $("[data-test-id='replan-notification']>.notification__content").
                 shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
         $(withText("Перепланировать")).click();
         $("[data-test-id='success-notification']>.notification__content").shouldBe(visible)
-                .shouldHave(exactText("Встреча успешно запланирована на " + (meeting2)), Duration.ofSeconds(15));
+                .shouldHave(exactText("Встреча успешно запланирована на " + (newMeetingDate)), Duration.ofSeconds(15));
     }
 }
